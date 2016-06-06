b9af1405d266b0ff6165fc5513b8f3baa55d653e
Kyle Ries <outtahere7@yahoo.com>
2016-06-06 15:58:45 -0400

    Tweaked error code

--- ShoppingListApp/ShoppingListApp.Models/ShoppingList/ShoppingListCreateModel.cs
+++ ShoppingListApp/ShoppingListApp.Models/ShoppingList/ShoppingListCreateModel.cs
@@ -14,7 +14,7 @@ namespace ShoppingListApp.Models
         public string Name { get; set; }
 
         [Required]
-        [RegularExpression("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$", ErrorMessage = "Can only input color codes")]
+        [RegularExpression("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$", ErrorMessage = "Can only input hex color codes")]
         [MinLength(7, ErrorMessage = "Please enter one # symbol followed by your hexadecimal color code.")]
         public string Color { get; set; }
 
